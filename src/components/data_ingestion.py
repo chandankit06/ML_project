463de03f051f227e5fc7d6460c95749682ee1e1d
chandankit06 <chandankit88@gmail.com>
2026-05-22 21:18:06 +0530

    Data Transformation Done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,10 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+
+from src.components.data_transformation import DataTransformationConfig
+
 @dataclass
 class DataIngestionConfig:
     train_data_path :str=os.path.join('artifacts','train.csv')
@@ -26,10 +30,13 @@ class DataIngestion:
             
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+
             logging.info('Train test split initiated')
             
             train_set,test_set= train_test_split(df,test_size=0.2,random_state=42)
+            
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
             logging.info('Ingestion of the data is completed')
@@ -39,7 +46,12 @@ class DataIngestion:
         except Exception as e:
             raise CustomException(e,sys)
         
-if __name__=='__main__':
-    obj=DataIngestion()
-    obj.inititate_data_ingestion()
 
+if __name__=='__main__':
+    obj= DataIngestion()
+    train_data,test_data=obj.inititate_data_ingestion()
+
+    data_transformation= DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+   
+  
